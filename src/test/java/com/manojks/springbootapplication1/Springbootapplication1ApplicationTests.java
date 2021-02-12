c34c058fbf323428f1cd7ffd9639481d1c2c44ce
manoj <manojks.masaurhi@gmail.com>
2021-02-12 13:29:02 +0530

    first commit

---
+++ src/test/java/com/manojks/springbootapplication1/Springbootapplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manojks.springbootapplication1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootapplication1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
